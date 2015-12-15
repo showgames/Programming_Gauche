6caed4d6da2fec023907df13e14d0732a9d988df
Shohei Hayashi <2329shohei@gmail.com>
2015-12-16 08:47:23 +0900

    first commit

--- simple_list_proc.scm
+++ simple_list_proc.scm
@@ -25,3 +25,45 @@
   (if (null? (cdr lis))
       lis
       (append2 (reverse (cdr lis)) (list (car lis)))))
+
+;; リストから条件を満たす要素をひとつ返す手続き
+(define (find pred lis)
+  (if (null? lis)
+      #f
+      (if (pred (car lis))
+	  (car lis)
+	  (find pred (cdr lis)))))
+
+;; ifではなくcondを使って書いてみる
+(define (find pred lis)
+  (cond
+   ((null? lis) #f)
+   ((pred (car lis)) (car lis))
+   (else
+    (find pred (cdr lis)))))
+
+;; リストの長さ
+(define (length lis)
+  (cond
+   ((null? lis) 0)
+   (else
+    (+ 1 (length (cdr lis))))))
+
+;; リストの中から、条件を満たす要素だけを抜き出したリストを返す
+(define (filter pred lis)
+  (cond
+   ((null? lis) '())
+   ((pred (car lis))
+    (cons (car lis) (filter pred (cdr lis))))
+   (else
+    (filter pred (cdr lis)))))
+
+
+;; 末尾再帰で length を書いてみる
+(define (length lis)
+  (define (length-rec lis n)
+    (if (null? lis)
+	n
+	(length-rec (cdr lis) (+ n 1))))
+  (length-rec lis 0))
+
