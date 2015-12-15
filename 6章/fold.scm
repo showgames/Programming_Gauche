797752cbcc009b603a6fd83d02f9f0c215cb3d6e
Shohei Hayashi <2329shohei@gmail.com>
2015-12-15 16:42:25 +0900

    first commit

--- 6章/fold.scm
+++ 6章/fold.scm
@@ -5,3 +5,31 @@
 
 (define (product-of-numbers lis)
   (fold * 1 lis))
+
+
+;; -inf.0 は負の無限大
+(define (max-number lis)
+  (define (pick-greater a b)
+    (if (> a b) a b))
+  (fold pick-greater -inf.0 lis))
+
+
+;; もう一つのmax-number
+(define (max-number lis)
+  (define (pick-greater a b)
+    (if (> a b) a b))
+  (if (null? lis)
+      (error "max-number needs at least one number")
+      (fold pick-greater (car lis) (cdr lis))))
+
+(define (length lis)
+  (define (increment2 a b) (+ b 1))
+  (fold increment2 0 lis))
+
+
+(define (print-elements lis)
+  (define (print-one-element a b) (print a))
+  (fold print-one-element #f lis))
+
+;; 要素を逆向きにしたリスト返す
+(fold cons '() '(a b c d e))
