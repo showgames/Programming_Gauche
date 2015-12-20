f570231336bcf256871710131fd5f302c8b0891d
Shohei Hayashi <2329shohei@gmail.com>
2015-12-20 10:21:38 +0900

    first commit

--- 7章/let.scm
+++ 7章/let.scm
@@ -22,3 +22,29 @@
 (letrec ((a 1)
 	 (b (lambda (x) (+ a x))))
   (b 4))
+
+;; letrecはローカルな再帰手続きを書くために有用
+(letrec ((sum (lambda (lis)
+		(cond
+		 ((null? lis) 0)
+		 ((number? (car lis)) (+ (car lis) (sum (cdr lis))))
+		 (else
+		  (sum (cdr lis)))))))
+  (sum '(1 3 #f 6 #t 9)))
+
+
+(letrec ((even? (lambda (n)
+		  (cond
+		   ((= n 0) #t)
+		   ((> n 0) (odd? (- n 1)))
+		   (else
+		    (odd? (+ n 1))))))
+	 (odd? (lambda (n)
+		 (cond
+		  ((= n 0) #f)
+		  ((> n 0) (even? (- n 1)))
+		  (else
+		   (even? (+ n 1)))))))
+  (even? 10))
+
+
